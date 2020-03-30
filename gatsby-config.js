b0eba7a643affc15ce70996c42bad178ee6db7bf
Philip <philip.sav@gmail.com>
2020-03-30 21:58:08 +0530

    Updated APIurl config.add package.json to gitignore

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-strapi',
       options: {
+        apiURL: process.env.DEPLOY_URL ? 'https://strapi-gatsby-postgresql-blog.herokuapp.com' : 'http://localhost:1337/',
         apiURL: 'http://localhost:1337',
         contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
           'article',
